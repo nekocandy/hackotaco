d3587db5f71d40ef628eb0044a13e0a15cd380ac
Piyush <bhangalepiyush@gmail.com>
2023-07-22 19:52:09 +0530

    feat: basic user data

--- pages/user_data.py
+++ pages/user_data.py
@@ -2,7 +2,20 @@ import streamlit as st
 
 from streamlit_extras.switch_page_button import switch_page
 
+from lib.database.user import get_user_data
+from lib.types.User import User
+
 if "user_info" not in st.session_state or not st.session_state["user_info"]:
     switch_page("Home")
 
-st.write(st.session_state["user_info"].email)
+
+user_data: User = st.session_state["user_info"]
+db_user_data = get_user_data(user_data.nickname)
+
+st.image(user_data.picture, width=100)
+st.header(f"Welcome {user_data.name}!")
+college_name = st.text_input(
+    "College Name", value=db_user_data.college_name if db_user_data else ""
+)
+if college_name:
+    st.write(f"Your college name is {college_name}")
