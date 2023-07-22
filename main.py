752dfbfd774792e9a97c24843b45d0e134db1e15
Piyush <bhangalepiyush@gmail.com>
2023-07-22 12:27:50 +0530

    feat: add auth

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 from auth0_component import login_button
 import streamlit as st
 
-clientId = "1DGyKVUR5gelgxFd4sPt4zoirf9L1cGu"
-domain = "hackshacks.us.auth0.com"
+clientId = st.secrets["auth"]["client_id"]
+domain = st.secrets["auth"]["domain"]
 
 user_info = login_button(clientId, domain = domain)
 st.write(user_info)
-
-st.sidebar.header("User Info")
-st.sidebar.button("Refresh")
-st.sidebar.date_input("Date of Birth")
