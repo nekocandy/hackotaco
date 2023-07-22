24ae9f670305bdbc8397f44b3a74eed3dd0cfb91
Piyush <bhangalepiyush@gmail.com>
2023-07-22 13:33:38 +0530

    feat: set user info in session

--- main.py
+++ main.py
@@ -4,5 +4,7 @@ import streamlit as st
 clientId = st.secrets["auth"]["client_id"]
 domain = st.secrets["auth"]["domain"]
 
-user_info = login_button(clientId, domain = domain)
-st.write(user_info)
+if "user_info" not in st.session_state or not st.session_state["user_info"]:
+  user_info = login_button(clientId, domain = domain)
+  st.session_state["user_info"] = user_info
+
