1dcda16a2202719be5b0d802b376ec4eb60a0768
Piyush <bhangalepiyush@gmail.com>
2023-07-22 14:54:50 +0530

    feat: custom pages and more stugg

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from auth0_component import login_button
 import streamlit as st
 
-clientId = st.secrets["auth"]["client_id"]
-domain = st.secrets["auth"]["domain"]
+from st_pages import Page, show_pages, add_page_title
 
-if "user_info" not in st.session_state or not st.session_state["user_info"]:
-  user_info = login_button(clientId, domain = domain)
-  st.session_state["user_info"] = user_info
+add_page_title("GG!")
 
+show_pages(
+    [
+        Page("./pages/home.py", "Home", "🏠"),
+        Page("./pages/hello.py", "Hello", "👋"),
+    ]
+)
