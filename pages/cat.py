6e90231de76d138e9c61db26e2d8cbda3078ef7a
codeash24 <o.a.aishu@gmail.com>
2023-07-22 19:47:20 +0530

    repo access

--- pages/cat.py
+++ pages/cat.py
@@ -1,6 +1,9 @@
 import streamlit as st
 from streamlit_extras.colored_header import colored_header
 from streamlit_extras.let_it_rain import rain
+import requests
+from urllib.parse import parse_qs, urlparse
+
 rain(
     emoji="💻",
     font_size=40,
@@ -9,10 +12,19 @@ rain(
 )
 
 user_info = st.session_state["user_info"]
-st.write(user_info.name)
 
 colored_header(
-    label='Welcome to Hack-o-Tacko',
+    label=f"Hack-o-Tacko @{user_info.name}",
     description="We at Hack-o-Tacko wish to enable hackers to navigate freely through the entirety of the hackathon  without having too much hassle",
     color_name="yellow-70",
 )
+
+repo_name = st.text_input('Name of repository', 'Repository Name')
+
+response = requests.get(f"https://api.github.com/repos/{user_info.nickname}/{repo_name}/commits?per_page=1")
+
+if response.status_code == 200:
+  st.write("Repository you are hacking with this weekend is", repo_name)
+else:
+  st.write(f"{response.status_code} repository not found")
+  
