b832a7a9d50bf6ea0011534b55806554b6a2352f
codeash24 <o.a.aishu@gmail.com>
2023-07-22 20:55:51 +0530

    change header

--- pages/user_data.py
+++ pages/user_data.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+from streamlit_extras.colored_header import colored_header
 from streamlit_extras.switch_page_button import switch_page
 
 from lib.database.user import get_user_data
@@ -13,7 +13,12 @@ user_data: User = st.session_state["user_info"]
 db_user_data = get_user_data(user_data.nickname)
 
 st.image(user_data.picture, width=100)
-st.header(f"Welcome {user_data.name}!")
+colored_header(
+    label=f"Welcome @{user_data.name}",
+    description="Fill in the details to auto-fill into the devpost",
+    color_name="green-70",
+)
+# st.header(f"Welcome {user_data.name}!")
 college_name = st.text_input(
     "College Name", value=db_user_data.college_name if db_user_data else ""
 )
