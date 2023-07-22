0e259496563782c05617e68cdc303e58e7ce6412
codeash24 <o.a.aishu@gmail.com>
2023-07-22 18:43:22 +0530

    diff page

--- pages/cat.py
+++ pages/cat.py
@@ -1,16 +1,18 @@
 import streamlit as st
 from streamlit_extras.colored_header import colored_header
 from streamlit_extras.let_it_rain import rain
-
 rain(
-    emoji="$",
+    emoji="💻",
     font_size=40,
     falling_speed=10,
     animation_length="infinite",
 )
 
+user_info = st.session_state["user_info"]
+st.write(user_info.name)
+
 colored_header(
-    label="Welcome to Hack-o-Tacko",
-    description="We at HackOTackO wish to enable hackers to navigate freely through the entirety of the hackathon  without having too much hassle",
+    label='Welcome to Hack-o-Tacko',
+    description="We at Hack-o-Tacko wish to enable hackers to navigate freely through the entirety of the hackathon  without having too much hassle",
     color_name="yellow-70",
 )
