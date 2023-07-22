095a4e3859c2722694b13f786a1a8bfcd195368f
Piyush <bhangalepiyush@gmail.com>
2023-07-22 12:27:38 +0530

    deps: add auth

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+from auth0_component import login_button
 import streamlit as st
 
-st.header("Hello")
+clientId = "1DGyKVUR5gelgxFd4sPt4zoirf9L1cGu"
+domain = "hackshacks.us.auth0.com"
+
+user_info = login_button(clientId, domain = domain)
+st.write(user_info)
+
+st.sidebar.header("User Info")
+st.sidebar.button("Refresh")
+st.sidebar.date_input("Date of Birth")
