45cbbcd3ba277bdffb7f002ac78e6f751cd00655
codeash24 <o.a.aishu@gmail.com>
2023-07-23 09:09:45 +0530

    remove unnecessary imports

--- pages/vd.py
+++ pages/vd.py
@@ -2,8 +2,6 @@ import streamlit as st
 from streamlit_extras.colored_header import colored_header
 from streamlit_extras.switch_page_button import switch_page
 from lib.types.User import User
-from streamlit_player import st_player
-from streamlit_extras.stoggle import stoggle
 
 if "user_info" not in st.session_state or not st.session_state["user_info"]:
     switch_page("Home")
