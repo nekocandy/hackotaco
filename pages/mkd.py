e5e787792323efbb6bb0afd3c81ee60023674530
Piyush <bhangalepiyush@gmail.com>
2023-07-23 09:59:40 +0530

    feat: add default content and increase width

--- pages/mkd.py
+++ pages/mkd.py
@@ -4,7 +4,8 @@ from streamlit_extras.switch_page_button import switch_page
 
 if "user_info" not in st.session_state or not st.session_state["user_info"]:
     switch_page("Home")
-    
+
+st.set_page_config(layout="wide") 
 user_info = st.session_state["user_info"]
 
 colored_header(
@@ -13,13 +14,27 @@ colored_header(
     color_name="violet-70",
 )
 
-col1, col2 = st.columns(2)
+default_value = """
+## Inspiration
+
+## What it does
+
+## How we built it
+
+## Challenges we ran into
+
+## Accomplishments that we're proud of
+
+## What we learned
+
+## What's next for HackerHacker
+"""
+
+col1, col2 = st.columns(2, gap="medium")
 with col1:
-  st.write("Markdown editor")
-  txt = st.text_area("",value="",height=250)
-  # st.markdown(st.text_area("Description", "Description"))
+    st.write("Markdown editor")
+    txt = st.text_area("", value=default_value.strip(), height=600)
 
 with col2:
-  st.write("Rendered .md")
-  st.markdown(txt)
-
+    st.write("Rendered .md")
+    st.markdown(txt)
