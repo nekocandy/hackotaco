df2bf0e2cdd2470ec915e44f85f2ec719d6c62a6
Piyush <bhangalepiyush@gmail.com>
2023-07-23 11:34:06 +0530

    feat: add cards

--- pages/teams/list.py
+++ pages/teams/list.py
@@ -1,9 +1,9 @@
 import streamlit as st
 from streamlit_extras.colored_header import colored_header
 from streamlit_extras.switch_page_button import switch_page
-from nanoid import generate
-from lib.database.teams import create_team, list_teams
+from streamlit_extras.metric_cards import style_metric_cards
 
+from lib.database.teams import list_teams
 from lib.types.User import User
 
 if "user_info" not in st.session_state or not st.session_state["user_info"]:
@@ -18,5 +18,11 @@ colored_header(
 )
 
 projects = list_teams(user_info.nickname)
+cols = st.columns(len(projects))
 
-st.write(projects)
+for col in range(len(cols)):
+    project = projects[col]
+    column = cols[col]
+    column.metric(label="Name", value=project["team_name"], delta=project["team_id"])
+
+style_metric_cards()
